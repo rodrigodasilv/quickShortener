a27c638b9b73449f56c2c30fdadf0292023dec94
Rodrigo <55567123+rodrigodasilv@users.noreply.github.com>
2024-12-17 23:03:50 -0300

    Added verification incase URL code is already used

--- URLManager.py
+++ URLManager.py
@@ -6,11 +6,19 @@ import redis
 redis_client = redis.Redis(host='redis', port=6379, charset="utf-8", decode_responses=True)
 
 
+def create_shorter_code():
+    return str(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16)))
+
+
 def get_url(shorter_code):
     return redis_client.get(shorter_code)
 
 
 def create_short_url(url):
-    shorter_code = str(''.join(random.SystemRandom().choice(string.ascii_uppercase + string.digits) for _ in range(16)))
+    while True:
+        shorter_code = create_shorter_code()
+        print(redis_client.get(shorter_code))
+        if redis_client.get(shorter_code) is None:
+            break
     redis_client.set(shorter_code, url)
     return json.dumps({"shorter_code": shorter_code})
